820143abc64beb1e849051272279c91df8743350
HiChild <1301662580@qq.com>
2020-11-01 15:21:38 +0800

    creat the project

---
+++ src/main/java/com/soe/langya/LangyaApplication.java
@@ -0,0 +1,13 @@
+package com.soe.langya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LangyaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LangyaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/soe/langya/LangyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soe.langya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LangyaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
